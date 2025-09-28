2d03c139c73a588930b760f69370d95b1a9a9278
leo <leoteste@gmail.com>
2025-09-28 12:33:18 -0300

    primeiro commit

---
+++ js.js
@@ -0,0 +1,7 @@
+function cliq() {
+    window.open('https://wa.me/5541998338242', '_blank');
+}
+
+function insta() {
+    window.open('https://www.instagram.com/onepiece_staff?utm_source=ig_web_button_share_sheet&igsh=cnVocXliaGN2cnB0', '_blank');
+}
